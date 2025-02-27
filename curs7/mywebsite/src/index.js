37422bff47f3aa2567c220398d51e2382eac41e5
Adela Mag <adela_mag@yahoo.com>
2025-02-27 20:48:45 +0200

    Vite

---
+++ curs7/mywebsite/src/index.js
@@ -0,0 +1,6 @@
+//Creeaza un obiect Date
+var currentDate = new Date();
+
+//obtine anul curent
+var currentYear = currentDate.getFullYear();
+document.getElementById('currentYear').textContent = currentYear;
